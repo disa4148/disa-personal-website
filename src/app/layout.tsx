400277f05d3f8a4c7ce82a1f428e8dab1433df50
disa4148 <denis.nikolaenko.2004@mail.ru>
2024-06-26 18:35:22 +0700

    remove next-intl

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,34 @@
-export default function RootLayout({
-  children,
-}: Readonly<{
+import type { Metadata } from 'next';
+import { Fira_Code } from 'next/font/google';
+
+import './globals.css';
+import Providers from '@/src/app/_providers/Providers';
+import { cn } from '@/src/shared/lib/utils';
+
+const FiraCode = Fira_Code({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'disa',
+  description:
+    'My personal portfolio site, which includes projects I ve worked on, my skills, and contacts',
+};
+
+interface LocaleLayoutProps {
   children: React.ReactNode;
-}>): React.ReactNode {
-  return children;
+  params: { locale: string };
 }
+
+const LocaleLayout: React.FC<LocaleLayoutProps> = async ({
+  children,
+  params: { locale },
+}) => {
+  return (
+    <html lang={locale} suppressHydrationWarning>
+      <body className={cn(FiraCode.className, 'bg-dark-bg-100')}>
+        <Providers>{children}</Providers>
+      </body>
+    </html>
+  );
+};
+
+export default LocaleLayout;
