2d372d3a77a79d34a3ebd2d925095d49102fae47
disa4148 <denis.nikolaenko.2004@mail.ru>
2024-07-17 14:51:26 +0700

    feat(app): multilanguage metadata generation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,31 +1,40 @@
+import './globals.css';
 import type { Metadata } from 'next';
 import { Fira_Code } from 'next/font/google';
-
-import './globals.css';
+import { NextIntlClientProvider, useMessages } from 'next-intl';
+import { getTranslations } from 'next-intl/server';
 import Providers from '@/src/app/_providers/Providers';
 import { cn } from '@/src/shared/lib/utils';
-import { NextIntlClientProvider } from 'next-intl';
-import { getLocale, getMessages } from 'next-intl/server';
+
 const FiraCode = Fira_Code({ subsets: ['latin'] });
 
-export const metadata: Metadata = {
-  title: 'disa',
-  description:
-    'My personal portfolio site, which includes projects I ve worked on, my skills, and contacts',
-};
+export async function generateMetadata({
+  params,
+}: {
+  params: { locale: string };
+}): Promise<Metadata> {
+  const { locale } = params;
+  const t = await getTranslations({ locale, namespace: 'metadata' });
+  return {
+    title: t('title'),
+    description: t('description'),
+  };
+}
 
 interface LocaleLayoutProps {
   children: React.ReactNode;
+  params: { locale: string };
 }
 
-const LocaleLayout: React.FC<LocaleLayoutProps> = async ({ children }) => {
-  const locale = await getLocale();
-
-  const messages = await getMessages();
+const LocaleLayout: React.FC<LocaleLayoutProps> = ({
+  children,
+  params: { locale },
+}) => {
+  const messages = useMessages();
   return (
     <html lang={locale} suppressHydrationWarning>
       <body className={cn(FiraCode.className, 'bg-dark-bg-100')}>
-        <NextIntlClientProvider messages={messages}>
+        <NextIntlClientProvider locale={locale} messages={messages}>
           <Providers>{children}</Providers>
         </NextIntlClientProvider>
       </body>
